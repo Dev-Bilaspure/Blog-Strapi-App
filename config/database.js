7979d853d65e00888f4afd5e3aa1d073ec443b35
Dev Bilaspure <devbilaspure4941@gmail.com>
2022-09-04 14:39:01 +0530

    remove password

--- config/database.js
+++ config/database.js
@@ -18,9 +18,9 @@ module.exports = ({ env }) => ({
     connection: {
       host: env('DATABASE_HOST', 'localhost'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'blogapp'),
+      database: env('DATABASE_NAME', '<database_name>'),
       user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'usingsql123'),
+      password: env('DATABASE_PASSWORD', '<database_password>'),
       schema: env('DATABASE_SCHEMA', 'public'), // Not required
       ssl: false
     },
