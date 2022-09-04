907b90c5d60c94fcf25fae8fad61ff1635dd04fa
Dev Bilaspure <devbilaspure4941@gmail.com>
2022-09-04 14:33:03 +0530

    first commit

---
+++ config/database.js
@@ -0,0 +1,41 @@
+module.exports = ({ env }) => ({
+  // connections: {
+  //   default: {
+  //     connector: "bookshelf",
+  //     settings: {
+  //       client: "postgres",
+  //       host: env("DATABASE_HOST", "localhost"),
+  //       port: env.int("DATABASE_PORT", 5432),
+  //       database: env("DATABASE_NAME", "blogapp"),
+  //       user: env("DATABASE_USERNAME", "postgres"),
+  //       password: env("DATABASE_PASSWORD", "strapi"),
+  //       ssl: false,
+  //     },
+  //   },
+  // },
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', 'localhost'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'blogapp'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD', 'usingsql123'),
+      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      ssl: false
+    },
+    debug: false,
+  },
+});
+
+// const path = require('path');
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'sqlite',
+//     connection: {
+//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
